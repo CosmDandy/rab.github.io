45a1c0cc871383ace19904ab415a6635fd9518e8
cosmdandy <tkondrashin@icloud.com>
2024-06-02 23:08:34 +0300

    ADD: New structure

--- backend/main.py
+++ backend/main.py
@@ -30,19 +30,16 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-@dp.message(Command('hello'))
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
+@dp.message(Command('switch'))
+async def switch_role(message: Message) -> None:
+    # TODO document why this method is empty
+    pass
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+
+@dp.message(Command('new_task'))
+async def new_task(message: Message) -> None:
+    # TODO document why this method is empty
+    pass
 
 
 async def main() -> None:
