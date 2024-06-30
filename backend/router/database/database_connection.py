c9ae07aa33f5fb29018a57e65b83ce573312f0f9
cosmdandy <tkondrashin@icloud.com>
2024-06-30 18:10:52 +0300

    ADD: Improve design

--- backend/router/database/database_connection.py
+++ backend/router/database/database_connection.py
@@ -2,8 +2,15 @@ from os import getenv
 
 import clickhouse_connect
 from aiogram import Router
+from aiogram.types import WebAppInfo
 
 router = Router(name=__name__)
 
 client = clickhouse_connect.get_client(host='clickhouse', port=8123, username=getenv('CLICKHOUSE_USER'),
                                        password=getenv('CLICKHOUSE_PASSWORD'), database='go')
+
+web_app = WebAppInfo(url='https://cosmdandy.github.io/rab.github.io/frontend/index.html')
+
+@router.message( )
+async def get_web_app(message):
+    await message.answer(text='Here is the link to the web app:', reply_markup=web_app)
