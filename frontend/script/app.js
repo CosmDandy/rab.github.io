6b1713fcaf613bf043d62dc2f0d6a7a921db6d58
cosmdandy <tkondrashin@icloud.com>
2024-06-30 19:16:00 +0300

    FIX: Add background folder

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -2,8 +2,8 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.setBackgroundColor('rgba(255,118,6,0.71)');
-tg.backgroundColor = 'rgba(255,118,6,0.71)';
+// tg.setBackgroundColor('rgba(255,118,6,0.71)');
+// tg.backgroundColor = 'rgba(255,118,6,0.71)';
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
